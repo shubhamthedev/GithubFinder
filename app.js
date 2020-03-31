37aa8fd39d33dcc2cd80cb029835461b15421ce8
Shubham Sharma <shubham.sharma1998@gmail.com>
2020-03-31 23:51:55 +0530

    Finished the project

---
+++ app.js
@@ -0,0 +1,25 @@
+//instantiate github
+const github = new Github;
+const ui = new UI;
+//Search input
+const searchUser = document.getElementById('searchUser');
+searchUser.addEventListener('keyup', (e) => {
+    //get input text
+    const userText = e.target.value;
+    if (userText !== '') {
+        //make http call
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    //show alert 
+                    ui.showAlert('User not found', 'alert alert-danger');
+                } else {
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                }
+            })
+    } else {
+        //clear profile
+        ui.clearProfile();
+    }
+});
